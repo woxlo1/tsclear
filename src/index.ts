8950e3b04e6eebd019b04701993ab7cdb2990b12
woxlo1 <satoshi00808@outlook.com>
2026-07-01 17:22:09 +0900

    feat: expose public library API from src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { parseTscOutput } from "./parser.js";
+export { explainDiagnostic, explainAll } from "./explainer.js";
+export { patterns } from "./patterns.js";
+export type { ErrorPattern, ParsedDiagnostic, ClearedDiagnostic } from "./types.js";
